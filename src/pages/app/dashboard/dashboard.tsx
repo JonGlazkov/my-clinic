786eed6996175d2d9292dbd95d5e92c484dbe741
Jon Ventura <jonathan_vmaced@hotmail.com>
2025-01-25 01:18:39 -0300

    [FEAT]: added dashboard and 404 page

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,31 @@
+import { Helmet } from 'react-helmet-async'
+
+import DayConsultsAmountCard from './day-consults-amount'
+import MonthConsultsAmountCard from './month-consults-amount'
+import MonthConsultsCanceledAmountCard from './month-consults-canceled-amount'
+import MonthRevenueCard from './month-revenue-card'
+import { PopularMedicsChart } from './popular-medics-charts'
+import { RevenueChart } from './revenue-chart'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthConsultsAmountCard />
+          <DayConsultsAmountCard />
+          <MonthConsultsCanceledAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
+          <PopularMedicsChart />
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/pages/app/dashboard/month-consults-amount.tsx
@@ -0,0 +1,24 @@
+import { ClipboardPlus } from 'lucide-react'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+export default function MonthConsultsAmountCard() {
+  return (
+    <Card className="bg-background">
+      <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-base font-semibold">
+          Consultas (mês)
+        </CardTitle>
+        <ClipboardPlus className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+
+      <CardContent className="space-y-1">
+        <span className="text-2xl font-bold tracking-tight">245</span>
+        <p className="text-xs text-muted-foreground">
+          <span className="text-emerald-500 dark:text-emerald-400">+7%</span> em
+          relação ao mês passado
+        </p>
+      </CardContent>
+    </Card>
+  )
+}
